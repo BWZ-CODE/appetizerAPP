f7455c8620049512d7b2d6e649b47ea804226436
BWZ-CODE <mmige24@gmail.com>
2025-10-10 12:25:42 -0500

    Initial Commit

---
+++ appetizerAPP/appetizerAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizerAPPApp.swift
+//  appetizerAPP
+//
+//  Created by Miguel Angel Bohorquez on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct appetizerAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
